8a6390e74269bc11c66f78a4cdd7d762db00f291
Daniel-cmr <danielcamara261@gmail.com>
2026-01-07 09:28:50 -0300

    Confirmações no Cadastro

--- PHP/formulario.php
+++ PHP/formulario.php
@@ -5,6 +5,17 @@ if(isset($_POST['salvar'])) {
     $nome = $_POST['nome'];
     $email = $_POST['email'];
     $senha= $_POST['senha'];
+    $confirmar_senha= $_POST['confirmar_senha'];
+    
+    if($senha !== $confirmar_senha) {
+        die("As senhas não coincidem.");
+    }
+
+    if (strlen($senha) < 6) {
+        die("A senha deve ter no mínimo 6 caracteres.");
+    }
+
+
 cadastrar_usuario($nome,$email,$senha);
 }
 ?>
